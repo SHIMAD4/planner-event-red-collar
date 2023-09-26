180e1e28f74628b036cad0fb6d8ddd4ac831883e
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-09-26 21:52:35 +0500

    add: Added logic for returning events from api

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,35 +1,39 @@
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
+import { useEffect, useState } from 'react';
 import './assets/css/Calendar.css';
 
 export default function Calendar() {
-  const events = [
-    { title: 'Final deadline', start: new Date(2023, 9, 20) },
-    { title: 'Final deadliadsadsadsadsadsadsadsasdasadsadsssd', start: new Date(2023, 8, 20) },
-    { title: 'Final deadliadsadsadsadsadsadsadsasdasadsadsssd', start: new Date(2023, 8, 19) },
-    { title: 'Final deadliadsadsadsadsadsadsadsasdasadsadsssd', start: new Date(2023, 8, 18) },
-  ]
-
-  const handleDateClick = (arg) => {
-    console.log(arg);
-  }
-  
+  const [events, setEvents] = useState([])
   const headerToolbar = {
     start: '',
     center: '',
     end: 'title prev next authButton'
   }
-
   const authButton = {
     text: 'Войти',
     click: function() {
       alert('Вход');
     }
   }
-  
+
+  useEffect(() => {
+    getData().then(data => {
+      data.forEach(elem => {
+        elem.start = elem.dateStart.split('T')[0]
+      })
+      setEvents(data)
+    })
+  }, [])
+
+  const handleDateClick = (arg) => {
+    console.log(arg)
+  }
+
   return (
     <FullCalendar
+<<<<<<< Updated upstream
       className="calendar"
       plugins={[ dayGridPlugin, interactionPlugin ]}
       dateClick={ handleDateClick }
@@ -43,8 +47,40 @@ export default function Calendar() {
       locale= 'ru'
       height={ 851 }
       customButtons= {{ authButton }}
+=======
+      plugins={[dayGridPlugin, interactionPlugin]}
+      dateClick={handleDateClick}
+      initialView="dayGridMonth"
+      selectable={true}
+      editable={true}
+      events={events}
+      eventContent={renderEventContent}
+      headerToolbar={headerToolbar}
+      titleFormat={{ month: 'long' }}
+      locale="ru"
+      height={851}
+      customButtons={{ authButton }}
+      firstDay={1}
+>>>>>>> Stashed changes
     />
   )
+}
+
+async function getData() {
+  try {
+    const response = await fetch('https://planner.rdclr.ru/api/events', {
+      method: 'GET',
+      headers: {
+        Authorization:
+          'Bearer be60dcc0d4d62b89123c4e982cd40eb40dcc59d58498f5b30df0b4f56ec59e8e1ac1ab912f6cca0fe2a9f6bf6f16c2685a52e92a7f9bcfe92d757d258c19ec495e346577def4052bf3ae917cd95ed3acb7cdb1985e72075770fcab4b13d05dc7301819b735445f29fcb97d6e6927986e34008970bb38c051a8a6a3b9dce5375d',
+      },
+    })
+    const json = await response.json()
+    return json.data
+  } catch (err) {
+    console.log(err)
+    return []
+  }
 }
 
 function renderEventContent(eventInfo) {
