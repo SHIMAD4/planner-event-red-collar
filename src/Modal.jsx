7088b8986a0c4eba4926a87fd2dd114e495144f3
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-09-28 16:04:43 +0500

    refactor: Slider moved to separate component

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,12 +1,6 @@
-import { useRef } from 'react';
-import { Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import arrow from './assets/icons/arrow.svg';
+import Slider from './Slider';
 import closeIcon from './assets/icons/close.svg';
 import './assets/scss/Modal.scss';
-import './assets/scss/Slider.scss';
 
 export default function Modal({ event, onClose }) {
   console.log("modal: ", event)
@@ -16,18 +10,7 @@ export default function Modal({ event, onClose }) {
   const time = start.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
   const description = event.extendedProps.description
   const location = event.extendedProps.location
-  const photos = []
   let isPast = event.classNames[0] === 'past' ? true : false
-
-  const navigationPrevRef = useRef(null)
-  const navigationNextRef = useRef(null)
-
-  if(event.extendedProps.photos) {
-    event.extendedProps.photos.forEach(elem => {
-      elem.src = 'https://planner.rdclr.ru' + elem.url
-      photos.push(elem)
-    })
-  }
   
   if(!event) {
     return null
@@ -51,33 +34,12 @@ export default function Modal({ event, onClose }) {
         </div>
         <h2 className='modal__people__desc'>Участники</h2>
         <div className="modal__people"></div>
-        <h2 className='modal__gallery__desc'>Галерея</h2>
-        <ul className='modal__gallery__list'>
-          <Swiper
-            modules={[Navigation]}
-            navigation={{
-              prevEl: navigationPrevRef.current,
-              nextEl: navigationNextRef.current,
-            }}
-            onBeforeInit={(swiper) => {
-                  swiper.params.navigation.prevEl = navigationPrevRef.current;
-                  swiper.params.navigation.nextEl = navigationNextRef.current;
-            }}
-            slidesPerView={3.3}
-          >
-            {
-              photos.map((item, key) => {
-                return <SwiperSlide key={key}><li><img src={item.src} alt="" width={item.width} height={item.height}/></li></SwiperSlide>
-              })
-            }
-          </Swiper>
-          <div className="next" ref={navigationNextRef}>
-            <img src={arrow} alt="" />
-          </div>
-          <div className="prev" ref={navigationPrevRef}>
-            <img src={arrow} alt="" />
-          </div>
-        </ul>
+        {
+          event.extendedProps.photos ? <><h2 className='modal__gallery__desc'>Галерея</h2>
+            <ul className='modal__gallery__list'>
+              <Slider event={event.extendedProps ? event.extendedProps : null} />
+            </ul></> : null
+        }
         <p className='modal__auth'><a href=''>Войдите</a>, чтобы присоединиться к событию</p>
       </div>
     </div>
