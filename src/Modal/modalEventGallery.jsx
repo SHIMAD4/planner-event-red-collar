ab6b8037692261b5f0e2aade1b3f6d80c90edf46
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-10-04 00:45:33 +0500

    refactor: Changed modal window structure in the gallery

--- src/Modal/modalEventGallery.jsx
+++ src/Modal/modalEventGallery.jsx
@@ -1,13 +1,83 @@
-import Slider from "../Slider"
+import { useCallback, useRef, useState } from "react"
+import { Navigation, Pagination } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/scss"
+import "swiper/scss/navigation"
+import arrow from "../assets/icons/arrow.svg"
+import "../assets/scss/Modal.scss"
 
 export default function Gallery({ event }) {
+  const sliderRef = useRef(null)
+  const [pastPrev, setPastPrev] = useState(false)
+  const [pastNext, setPastNext] = useState(true)
+  let photos = []
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return
+    sliderRef.current.swiper.slideNext()
+  }, [])
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return
+    sliderRef.current.swiper.slidePrev()
+  }, [])
+
   return (
     <>
       {event.extendedProps.photos ? (
         <>
-          <h2 className="modal__gallery__desc">Галерея</h2>
+          <div className="modal__gallery__header">
+            <h2 className="modal__gallery__desc">Галерея</h2>
+            {event.extendedProps.photos.length >= 4 ? (
+              <div className="modal__gallery__buttons">
+                <button
+                  className={pastPrev ? "next swiper-button-disabled" : "next"}
+                  onClick={handlePrev}>
+                  <img src={arrow} alt="" />
+                </button>
+                <button
+                  className={pastNext ? "prev swiper-button-disabled" : "prev"}
+                  onClick={handleNext}>
+                  <img src={arrow} alt="" />
+                </button>
+              </div>
+            ) : null}
+          </div>
           <ul className="modal__gallery__list" role="list">
-            <Slider event={event.extendedProps ? event.extendedProps : null} />
+            {event.extendedProps.photos.forEach((elem) => {
+              elem.src = "https://planner.rdclr.ru" + elem.url
+              photos.push(elem)
+            })}
+            <Swiper
+              modules={[Navigation, Pagination]}
+              ref={sliderRef}
+              navigation={{
+                prevEl: document.querySelector(".prev"),
+                nextEl: document.querySelector(".next"),
+              }}
+              pagination={{
+                el: ".swiper-pagination",
+                clickable: true,
+              }}
+              slidesPerView={photos.length >= 4 ? 3.3 : 1}
+              slideToClickedSlide={true}
+              onSlideChange={(swiper) => {
+                const lastSlideIndex = swiper.realIndex
+                lastSlideIndex > 0 ? setPastPrev(true) : setPastNext(true)
+              }}>
+              {photos.map((item, key) => {
+                return (
+                  <SwiperSlide key={key}>
+                    <li>
+                      <img className="slide-photo" src={item.src} alt="" />
+                    </li>
+                  </SwiperSlide>
+                )
+              })}
+              {photos.length >= 4 ? (
+                <div className="swiper-pagination"></div>
+              ) : null}
+            </Swiper>
           </ul>
         </>
       ) : null}
--- src/Modal/modalEventParticipants.jsx
+++ src/Modal/modalEventParticipants.jsx
@@ -6,17 +6,15 @@ export default function Participants({ event }) {
 
   if (participants.length === 0) return null
 
-  console.log("participants", participants)
-
   return (
     <>
       <h2 className="modal__people__desc">Участники</h2>
       <ul className="modal__people" role="list">
         {participants.map((item, index) => {
           return index < 4 ? (
-            <li key={index}>
+            <li key={self.crypto.randomUUID()}>
               <img src={avatar} alt="" />
-              <div className="">
+              <div className="modal__people__info">
                 <p className="modal__people__username">{item.username}</p>
                 {+event.id === item.id ? (
                   <p className="modal__people__role">Организатор</p>
@@ -24,15 +22,12 @@ export default function Participants({ event }) {
               </div>
             </li>
           ) : index > 4 ? (
-            // for(let i = 0; i <= 3; i++) {
-            //   participants[i].photos
-            // }
-            <>
+            <div key={self.crypto.randomUUID()}>
               <img className="photo1" src={avatar} alt="" />
               <img className="photo2" src={avatar} alt="" />
               <img className="photo3" src={avatar} alt="" />
               <p className="count">Eще +{participantsCount - 4}</p>
-            </>
+            </div>
           ) : null
         })}
       </ul>
