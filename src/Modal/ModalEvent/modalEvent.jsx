4cb831618d31a3541d2d9d0368f65918af9c957c
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-10-05 13:09:06 +0500

    add: Added the ability to open a modal to enter from an event

--- src/Modal/ModalEvent/modalEvent.jsx
+++ src/Modal/ModalEvent/modalEvent.jsx
@@ -1,20 +1,49 @@
+import { useState } from "react"
 import "../../shared/scss/Modal/ModalEvent/ModalEvent.scss"
 import Modal from "../Modal"
+import ModalAuth from "../ModalAuth/modalAuth"
 import ModalEventGallery from "./modalEventGallery"
 import ModalEventInfo from "./modalEventInfo"
 import ModalEventParticipants from "./modalEventParticipants"
 
 export default function ModalEvent({ event, onClose, isOpen }) {
+  const [auth, setAuth] = useState(false)
+  const [firstModalOpen, setFirstModalOpen] = useState(true)
+
   if (!event) return null
 
+  const closeFirstModal = () => {
+    setFirstModalOpen(false)
+  }
+
   return (
-    <Modal onClose={onClose} isOpen={isOpen} title={event.title}>
-      <ModalEventInfo event={event} />
-      <ModalEventParticipants event={event} />
-      <ModalEventGallery event={event} />
-      <p className="modal__auth">
-        <a href="">Войдите</a>, чтобы присоединиться к событию
-      </p>
-    </Modal>
+    <>
+      {firstModalOpen && (
+        <Modal onClose={onClose} isOpen={isOpen} title={event.title}>
+          <ModalEventInfo event={event} />
+          <ModalEventParticipants event={event} />
+          <ModalEventGallery event={event} />
+          <p className="modal__auth">
+            <a
+              onClick={() => {
+                setAuth(true)
+                closeFirstModal()
+              }}>
+              Войдите
+            </a>
+            , чтобы присоединиться к событию
+          </p>
+        </Modal>
+      )}
+      {auth && (
+        <ModalAuth
+          onClose={() => {
+            setAuth(false)
+            setFirstModalOpen(true)
+          }}
+          isOpen={true}
+        />
+      )}
+    </>
   )
 }
