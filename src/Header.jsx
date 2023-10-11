4f1c495c61763f1fe5b249e64162168306fae9c5
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-10-11 20:26:57 +0300

    refactor: Changed calendar styles and added variables and mixins

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,15 +4,9 @@ export default function Header() {
   return (
     <>
       <header className="header">
-        <img
-          className="header__logo"
-          src={logo}
-          alt="logo"
-          width={53}
-          height={31}
-        />
-        <p className="header__alt">red collar</p>
-        <h1 className="header__heading">
+        <img className="header__logo" src={logo} alt="logo" width={53} height={31} />
+        <p className="header__logo__title">red collar</p>
+        <h1 className="header__logo__subtitle">
           planner <span>event</span>
         </h1>
       </header>
