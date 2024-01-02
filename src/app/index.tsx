a326f02e012b153c8007c63b97ac093555f136b7
Vitaliy Starcev <6asm6deus6@gmail.com>
2024-01-02 16:11:03 +0300

    feat(PE-2): add manifest & cache load

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,17 +3,27 @@ import '../shared/styles/main.scss';
 import { Calendar } from '../entities/calendar';
 import { Header } from '../widgets/header';
 
+window.addEventListener('load', async () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
+        console.log('Service worker register success');
+    } else {
+        console.log('Service worker register fail');
+    }
+    const rootElement = document.getElementById('root');
+
+    if (rootElement) {
+        createRoot(rootElement).render(<App />);
+    } else {
+        console.error('Root element not found!');
+    }
+
+    await App();
+});
+
 const App = () => (
     <>
         <Header />
         <Calendar />
     </>
 );
-
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-    createRoot(rootElement).render(<App />);
-} else {
-    console.error('Root element not found!');
-}
